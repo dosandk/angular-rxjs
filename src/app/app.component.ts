5c5751199c5fb5f9180cef3e15a38dfaf977bff4
Vladimir Shevchuk <dosandkv@gmail.com>
2018-08-19 00:53:25 +0300

    test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import {Component, OnInit} from '@angular/core';
-import {SchedulingService} from './scheduling.service';
+import { Component, OnInit } from "@angular/core";
+import { SchedulingService } from "./scheduling.service";
 
 interface Item {
   id: number;
@@ -7,43 +7,41 @@ interface Item {
 }
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent implements OnInit {
   data: Array<Item> = [];
 
-  constructor (
-    public schedulingService: SchedulingService,
-  ) {}
+  constructor(public schedulingService: SchedulingService) {}
 
-  ngOnInit () {
-    console.error('ngOnInit');
+  ngOnInit() {
+    console.error("ngOnInit");
     this.data = [
-      {id: 1, title: 'foo'},
-      {id: 2, title: 'bar'},
-      {id: 3, title: 'baz'}
+      { id: 1, title: "foo" },
+      { id: 2, title: "bar" },
+      { id: 3, title: "baz" }
     ];
   }
 
-  getItems () {
+  getItems() {
     this.schedulingService.getItems().subscribe((items: Array<Item>) => {
-      console.error('getItems subscribe', items);
+      console.error("getItems subscribe", items);
       this.data.push(...items);
     });
   }
 
-  setItem () {
+  setItem() {
     this.schedulingService.setItem().subscribe((item: Item) => {
-      console.error('setItem subscribe', item);
+      console.error("setItem subscribe", item);
       this.data.unshift(item);
     });
   }
 
-  deleteItem () {
+  deleteItem() {
     this.schedulingService.deleteItem(/*id*/).subscribe((item: Item) => {
-      console.error('deleteItem subscribe', item);
+      console.error("deleteItem subscribe", item);
       this.data = this.data.slice(1);
     });
   }
